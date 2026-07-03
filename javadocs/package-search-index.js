12789af06f7dd23573b763202f44a462482f83fb
Ethan E. <eastwoode375@gmail.com>
2026-07-03 18:17:06 -0500

    Update and Include Javadocs

---
+++ javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"inventorymgr.InventoryManagementSystem","l":"inventorymgr.InventoryManagementSystem"}];updateSearchResults();
